0facea360e0fdbf9f7e503d5b5c3018c3fc4fcc5
Melvyn Peignon <peignonmel@eisti.eu>
2016-12-14 14:13:35 +0800

    Clean a little bit the code and the architecture

--- jobs/youtubeAPI/get_youtubeAPI.py
+++ jobs/youtubeAPI/get_youtubeAPI.py
@@ -10,27 +10,27 @@ reload(sys)  # Reload does the trick!
 sys.setdefaultencoding('UTF8')
 MYDIR = os.path.dirname(os.path.abspath(__file__))
 
-def get_videoInfo(jsonData):
+def get_videoInfo(json_data):
 	with open(os.path.join(MYDIR, 'YoutubeCategories.json'), 'r') as f:
-		categoryDic = json.load(f)
+		category_dic = json.load(f)
 
-	jsonData = json.loads(jsonData)
-	queryString = urlparse(jsonData["url_youtube"]).query.split('=')[1]
+	json_data = json.loads(json_data)
+	video_id = urlparse(json_data["youtube_link"]).query.split('=')[1]
 
 	h = httplib2.Http(".cache")
-	(data, content) = h.request("https://www.googleapis.com/youtube/v3/videos?part=id,snippet,statistics&fields=items(statistics,snippet(publishedAt,title,description,categoryId,tags))&id=%s&key=%s" % (queryString, settings.YOUTUBE_API_KEY), "GET")
+	(data, content) = h.request("https://www.googleapis.com/youtube/v3/videos?part=id,snippet,statistics&fields=items(statistics,snippet(publishedAt,title,description,categoryId,tags))&id=%s&key=%s" % (video_id, settings.YOUTUBE_API_KEY), "GET")
 
 	if (data["status"] == '200' or data["status"] == '304'):
 		content = json.loads(content)
 		snippet = content["items"][0]["snippet"]
 		statistics = content["items"][0]["statistics"]
-		snippet["categoryId"] = categoryDic[snippet["categoryId"]]
+		snippet["categoryId"] = category_dic[snippet["categoryId"]]
 
-		jsonData.update(snippet)
-		jsonData.update({"statistics": statistics})
+		json_data.update(snippet)
+		json_data.update({"statistics": statistics})
 
 		with codecs.open(os.path.join(MYDIR, 'videoInfoData.json'), 'w', encoding='utf8') as handle:
-			handle.write(json.dumps(jsonData, indent=4, sort_keys=True, ensure_ascii=False).encode('utf8'))
+			handle.write(json.dumps(json_data, indent=4, sort_keys=True, ensure_ascii=False).encode('utf8'))
 
 		return "Finish youtube API crawler and file is videoInfoData.json."
 	else:
@@ -41,11 +41,11 @@ def get_categories():
 	h = httplib2.Http(".cache")
 	(data, content) = h.request("https://www.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=US&key=%s" % (settings.YOUTUBE_API_KEY), "GET")
 	data = json.loads(content)
-	categoryDic = {}
+	category_dic = {}
 	for key in data["items"]:
-		categoryDic[key["id"]] = key["snippet"]["title"]
+		category_dic[key["id"]] = key["snippet"]["title"]
 	with open(os.path.join(MYDIR, 'YoutubeCategories.json'), 'w') as outfile:
-		json.dump(categoryDic, outfile)
+		json.dump(category_dic, outfile)
 
 
 get_categories()
